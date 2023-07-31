6bb94aa3ffe8e9db84753960f6e6f2dc90d8b577
rhymald <rhymald@gmail.com>
2023-07-31 12:32:27 +0200

    ~ filter by distance

--- api/1_world.go
+++ api/1_world.go
@@ -43,25 +43,33 @@ func newWorld() *Location {
 func getAll(c *gin.Context) { 
 	ctx, span := tracer.Start((*c).Request.Context(), "pull-all-objects")
 	defer span.End()
+	// id := c.Param("id")
 
 	var buffer []play.Simplified
 	_, spanPlayers := tracer.Start(ctx, "players")
 	countOfPlayers, countOfFoes := 0, 0
-	world.Lock()
-	plimit, flimit := base.Round(math.Log2(float64(len((*world).ByID)))+1), base.Round(math.Sqrt(float64(len((*world).ByID))))
+	world.Lock() ; objLimit := len((*world).ByID)
+	plimit, flimit := base.Round(math.Log2(float64( objLimit ))+1), base.Round(math.Sqrt(float64( objLimit )))
+	first := [2]int{}// (*world).ByID[id].Path()[1]
 	for _, each := range (*world).ByID { 
+		distance := 0.0
+		if countOfFoes + countOfPlayers == 0 {
+			first = each.Path()[1]
+		} else {
+			distance = math.Sqrt( math.Pow(float64(each.Path()[1][0] - first[0]), 2) + math.Pow(float64(each.Path()[1][1] - first[1]), 2) )
+		}
 		if (*each).Current.IsNPC() == false { 
+			if countOfPlayers < plimit && distance < 500 { buffer = append(buffer, (*each).Current.Simplify(each.Path())) }
 			countOfPlayers++ 
-			if countOfPlayers < plimit { buffer = append(buffer, (*each).Current.Simplify(each.Path())) }
 		} else { 
+			if countOfFoes < flimit && distance < 500 { buffer = append(buffer, (*each).Current.Simplify(each.Path())) }
 			countOfFoes++ 
-			if countOfFoes < flimit { buffer = append(buffer, (*each).Current.Simplify(each.Path())) }
 		}
 		if countOfFoes + countOfPlayers >= plimit + flimit { break } 
 	} // ; if countOfPlayers < 10 {break}}
 	world.Unlock()
 	span.SetAttributes(attribute.Int("Players", countOfPlayers))
-	span.SetAttributes(attribute.Int("NPCs", len(buffer)-countOfPlayers))
+	span.SetAttributes(attribute.Int("NPCs", countOfFoes))
 	spanPlayers.End()
 
 	_, spanResponse := tracer.Start(ctx, "responding")
