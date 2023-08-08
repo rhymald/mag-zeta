c7ea8bc6abec64e2e863fe8a37e7d834baa63432
rhymald <rhymald@gmail.com>
2023-08-08 11:23:52 +0200

    + pretty log

--- api/0_api.go
+++ api/0_api.go
@@ -4,13 +4,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"encoding/json"
+	"fmt"
 )
+
+type LogEvent struct {
+	Status  int    `json:"Status"`
+	Time    string `json:"Time"`
+	Latency string `json:"Latency"`
+	Source  string `json:"Source"`
+	Method  string `json:"Method"`
+	Path    string `json:"Path"`
+}
 
 func RunAPI() {
 	// router := gin.Default()
 	router := gin.New()
 	router.Use(gin.Recovery()) // to recover gin automatically
-	router.Use(jsonLoggerMiddleware()) // we'll define it later
+	router.Use(jsonLoggerMiddleware())
 	router.GET("/", hiThere)
 	router.GET("/around", getAll)
 	router.GET("/around/:myplayerid", getAll)
@@ -20,7 +30,7 @@ func RunAPI() {
 	router.GET("/test/:id", showState)
 	metrics := gin.New()
 	metrics.Use(gin.Recovery()) // to recover gin automatically
-	metrics.Use(jsonLoggerMiddleware()) // we'll define it later
+	metrics.Use(jsonLoggerMiddleware())
 	metrics.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	go func(){ router.Run(":4917") }()
 	go func(){ metrics.Run(":9093") }()
@@ -38,13 +48,14 @@ func jsonLoggerMiddleware() gin.HandlerFunc {
 			if len(params.Path) >= 7 {
 				if params.StatusCode == 200 && params.Path[:7] == "/around" { return "" }
 			}
-			log := make(map[string]interface{})
-			log["method"] = params.Method
-			log["way"] = params.Path
-			log["remote_addr"] = params.ClientIP
-			log["response_time"] = params.Latency.String()
-			log["start_time"] = params.TimeStamp.Format("2006/01/02 - 15:04:05")
-			log["status_code"] = params.StatusCode
+			log := LogEvent{
+				Status:  params.StatusCode,
+				Latency: fmt.Sprintf("%0.3fms", float64(params.Latency.Microseconds())/1000),
+				Method:  params.Method,
+				Path:    params.Path,
+				Time:    params.TimeStamp.Format("2006/01/02 15:04:05.999"),
+				Source:  params.ClientIP,
+			}
  			s, _ := json.Marshal(log)
 			return string(s) + "\n"
 		},
