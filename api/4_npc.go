655d8a2bb7fd49d645c81242b67a749c69c86ad0
rhymald <rhymald@gmail.com>
2023-08-12 12:05:49 +0200

    It is testing func

--- api/4_npc.go
+++ api/4_npc.go
@@ -14,43 +14,6 @@ import (
 	"fmt"
 )
 
-func newFoe(c *gin.Context) { 
-	ctx, span := tracer.Start((*c).Request.Context(), "spawn-foe")
-	defer span.End()
-
-	_, spanGenerate := tracer.Start(ctx, "generating-basic-stats")
-	foe := play.MakeNPC()
-	spanGenerate.AddEvent(fmt.Sprintf("Character generated with ID: %s", foe.GetID()))
-	span.SetAttributes(attribute.String("CharacterID",foe.GetID()))
-	spanGenerate.End()
-
-	_, spanCalculate := tracer.Start(ctx, "calculating-attributes-from-basic")
-	err := foe.CalculateAttributes()
-	if err != nil { spanCalculate.RecordError(err) }
-	spanCalculate.End()
-
-	_, spanResponse := tracer.Start(ctx, "responding")
-	world.Lock()
-	state := foe.NewState()
-	if err == nil {
-		(*world).ByID[foe.GetID()] = state
-		c.IndentedJSON(200, struct{ ID string }{ ID: foe.GetID() })
-	} else {
-		c.AbortWithError(500, errors.New("Invalid foe character"))
-	}
-	world.Unlock()
-	spanResponse.End()
-
-	go func(){ Lifecycle_Regenerate(state, (*c).Request.Context()) }()
-	go func(){ Lifecycle_EffectConsumer(state, (*c).Request.Context()) }()
-	go func(){ for {
-		state.Move(0.11, false, GridCache)
-		// state.Turn(0.11, GridCache)
-	}}()
-	// go func(){ for {state.Turn(1/math.Phi/math.Phi * float64(base.Epoch()%3-1), GridCache)} }()
-	// go func(){ base.Wait(30000) ; state.Lock() ; connect.WriteTrace((*world).Writer, foe.GetID(), &(*state).Trace) ; state.Unlock() }()
-}
-
 // func npcRegen(hps *base.Life, ids *map[string]int, span *trace.Span) {
 // 	hp := 32
 // 	hps.HealDamage(hp)
